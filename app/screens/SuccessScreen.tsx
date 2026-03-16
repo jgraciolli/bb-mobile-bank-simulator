f9b5f90fd5850551c2e3908cf1e3b667945aa6f4
João Graciolli <148507173+jgraciolli@users.noreply.github.com>
2026-03-16 20:32:46 -0300

    feat: success screen template

--- app/screens/SuccessScreen.tsx
+++ app/screens/SuccessScreen.tsx
@@ -1,9 +1,15 @@
+import { navigate } from "expo-router/build/global-state/routing";
 import { Text, View } from "react-native";
+import { AppButton } from "../components";
+import { globalStyles } from "../styles/globalStyles";
 
 const SuccessScreen = () => {
   return (
-    <View>
-      <Text>Tela de sucesso</Text>
+    <View style={globalStyles.container}>      
+      <Text style={globalStyles.title}>Transferência realizada com sucesso!</Text>
+      <Text style={globalStyles.subtitle}>O valor foi enviado ao destinatário.</Text>
+
+      <AppButton title="Voltar ao início" onPress={() => navigate("/home")} />
     </View>
   );
 };
