97ddd69ad48144eeac1cd38f612bf355caeb205b
João Graciolli <148507173+jgraciolli@users.noreply.github.com>
2026-03-16 20:29:37 -0300

    feat: transfer screen template

--- app/screens/TransferScreen.tsx
+++ app/screens/TransferScreen.tsx
@@ -1,9 +1,33 @@
+import { navigate } from "expo-router/build/global-state/routing";
+import { useState } from "react";
 import { Text, View } from "react-native";
+import { AppButton, LabeledInput } from "../components";
+import { globalStyles } from "../styles/globalStyles";
 
 const TransferScreen = () => {
+  const [recipient, setRecipient] = useState("");
+  const [amount, setAmount] = useState("");
+
   return (
-    <View>
-      <Text>Tela de transferência</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.title}>Tela de transferência</Text>
+
+      <LabeledInput
+        label="Destinatário"
+        placeholder="Nome do destinatário..."
+        value={recipient}
+        onChangeText={setRecipient}
+      />
+
+      <LabeledInput
+        label="Valor"
+        placeholder="Valor a ser transferido..."
+        value={amount}
+        onChangeText={setAmount}
+        keyboardType="number-pad"
+      />
+
+      <AppButton title="Transferir" onPress={() => navigate("/success")} />
     </View>
   );
 };
